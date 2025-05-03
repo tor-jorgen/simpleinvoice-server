5b8ae1f7cd60cd8ad589aba22ae9caf755823c2b
Tor Jørgen Johannessen <tor.jorgen@johannessenweb.com>
2025-05-03 10:48:15 +0200

    Gradle cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,9 @@
-val h2_version: String by project
-val koin_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version: String by project
+val kotlinVersion: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.2"
-    kotlin("plugin.serialization") version "2.1.20"
-    id("org.flywaydb.flyway") version "10.0.0"
+    kotlin("jvm")
+    id("io.ktor.plugin")
+    kotlin("plugin.serialization")
 }
 
 group = "org.simpleinvoice.server"
@@ -58,15 +53,20 @@ dependencies {
 
     val kafkaVersion = "2.1.2"
     implementation("io.github.flaxoos:ktor-server-kafka:$kafkaVersion")
-//    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
 
-    // Database migration
+    val h2Version = "2.3.232"
+    implementation("com.h2database:h2:$h2Version")
+
+    val koinVersion = "3.5.6"
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
+    val logbackVersion = "1.4.14"
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
     val postgresVersion = "42.7.5"
     implementation("org.postgresql:postgresql:$postgresVersion")
+
     val flywayVersion = "11.7.0"
     implementation("org.flywaydb:flyway-core:$flywayVersion")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
@@ -78,17 +78,22 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
+    // Document conversion
     val odfDomVersion = "0.12.0"
     implementation("org.odftoolkit:odfdom-java:$odfDomVersion")
+
     val simpleOdfVersion = "0.9.0"
     implementation("org.odftoolkit:simple-odf:$simpleOdfVersion")
-    implementation("org.odt2pdf", "odt2pdf", "1.0", classifier = "all")
 
-    val javaxMail = "1.6.2"
-    implementation("com.sun.mail:javax.mail:$javaxMail")
+    val odt2pdfVersion = "1.0"
+    implementation("org.odt2pdf", "odt2pdf", odt2pdfVersion, classifier = "all")
+
+    // Email
+    val javaxMailVersion = "1.6.2"
+    implementation("com.sun.mail:javax.mail:$javaxMailVersion")
 
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
 
 ktor {
@@ -96,9 +101,3 @@ ktor {
         localImageName.set("simpleinvoice-server")
     }
 }
-
-// flyway {
-//    skipExecutingMigrations = true
-//    url = "jdbc:postgresql://localhost:5432/simple_invoice"
-//    user = "admin"
-// }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val ktorVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        id("io.ktor.plugin") version ktorVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
+}
+
 rootProject.name = "simpleinvoice-server"

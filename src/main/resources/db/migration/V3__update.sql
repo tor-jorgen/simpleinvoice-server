e2754832d69324d90ea7954aa31306bbbf89884f
Tor Jørgen Johannessen <tor.jorgen@johannessenweb.com>
2025-05-09 16:52:39 +0200

    Collected all database creation in one file

--- src/main/resources/db/migration/V3__update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE invoice
-    ADD COLUMN invoice_file_path VARCHAR;
--- src/main/resources/db/migration/V4__update.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE audit_trail
-(
-    id        UUID    NOT NULL PRIMARY KEY,
-    timestamp VARCHAR NOT NULL,
-    item_id   VARCHAR NOT NULL,
-    item      VARCHAR,
-    message   VARCHAR,
-    user_id   VARCHAR
-);

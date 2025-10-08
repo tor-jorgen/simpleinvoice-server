6c1a662bac604a34b8e53a276e1ceb3065e5ee69
Tor Jørgen Johannessen <tj@johannessenweb.com>
2025-10-08 23:10:58 +0200

    Store invoice documents in docker generated volume. Back up all data

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 
 help() {
   echo "backup.sh [file name]"
-  echo "Back up Simple Invoice database"
-  echo "  file name: Name of file to backup to. Default is 'simpleinvoice.zip'"
+  echo "Back up Simple Invoice data"
+  echo "  file name: Name of file to backup to. Default is 'simpleinvoice-<timestamp>.zip'"
   echo "Note! You need system administrator rights (sudo) to run this command, and you might have to enter your sudo password (depending on when you entered it the last time)"
 }
 
@@ -18,7 +18,10 @@ else
   FILE="$1"
 fi
 
-DIR=$(docker volume inspect simple-invoice-db-data | sed -n 's/.*"Mountpoint": "\([^"]*\)".*/\1/p')
-sudo zip -r "$FILE" "$DIR" >/dev/null 2>&1
-sudo chown "$USERNAME":"$USERNAME" "$FILE"
-echo backed up database to "$FILE"
+DB_DIR=$(docker volume inspect simple-invoice-db-data | sed -n 's/.*"Mountpoint": "\([^"]*\)".*/\1/p')
+DOC_DIR=$(docker volume inspect simple-invoice-documents | sed -n 's/.*"Mountpoint": "\([^"]*\)".*/\1/p')
+TIME=$(date --iso-8601=seconds)
+FILENAME="${FILE/.zip/}-$TIME.zip"
+sudo zip -r "$FILENAME" "$DB_DIR" "$DOC_DIR" ./.config .env >/dev/null 2>&1
+sudo chown "$USERNAME":"$USERNAME" "$FILENAME"
+echo backed up data to "$FILENAME"

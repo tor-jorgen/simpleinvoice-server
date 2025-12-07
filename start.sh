01d43ed2a0436b69fe5a8a428c8ec636fb8ef0af
Tor Jørgen Johannessen <tj@johannessenweb.com>
2025-12-07 01:29:46 +0100

    Update verification metadata

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 help() {
-  echo "start.sh [--no-daemon]"
+  echo "start.sh [--no-daemon] [--local-images] [--skip-build]"
   echo "Start Simple Invoice"
   echo "--no-daemon: Do not run containers as daemons. This makes the logs visible in the console"
-  echo "--skip-build: Do not build the application. This will make startup faster if you have already run the application"
+  echo "--local-images: This will build the images for server and app locally instead of using the prebuilt images from GitHub"
+  echo "--skip-build: Do not build the application. This will make Simple Invoice start up faster if you are using local images and have already built them"
   echo
-  echo "If the startup fails, it might be because you have to little memory on your computer. Try to close other programs while you start up. Simple Invoice has to be built the first time you start up, and that consumes quite some resources"
+  echo "If the startup fails, it might be because you have to little memory on your computer. Try to close other programs while you start up. Please not that building Simple Invoice locally consumes quite some resources"
   echo "Run './stop.sh' to stop the backend. If you started it with --no-daemon, you need to push Ctrl+C before you run './stop.sh'"
 }
 
@@ -37,20 +38,26 @@ fi
 
 create_config_dir
 
-if [[ "$1" == "--no-daemon" || "$2" == "--no-daemon" ]]; then
+if [[ "$1" == "--no-daemon" || "$2" == "--no-daemon" || "$3" == "--no-daemon" ]]; then
   DAEMON=
   show_info
 else
   DAEMON=-d
 fi
 
-if [[ "$1" == "--skip-build" || "$2" == "--skip-build" ]]; then
+if [[ "$1" == "--skip-build" || "$2" == "--skip-build" || "$3" == "--skip-build" ]]; then
   echo "Skipping build"
-else
-  docker compose build
+elif [[ "$1" == "--local-images" || "$2" == "--local-images" || "$3" == "--local-images" ]]; then
+  docker compose -f compose-all.yaml build --no-cache
 fi
 
-docker compose up $DAEMON
+if [[ "$1" == "--local-images" || "$2" == "--local-images" || "$3" == "--local-images" ]]; then
+  COMPOSE_FILE="-f compose-all.yaml"
+else
+  COMPOSE_FILE=
+fi
+
+docker compose "$COMPOSE_FILE" up $DAEMON
 
 if [ "$DAEMON" == "-d" ]; then
   echo
